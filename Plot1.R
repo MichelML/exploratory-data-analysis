9f58772ab8b680827d7379880996b56d7c5e8cbd
Michel Moreau <michmoreau.l@gmail.com>
2014-11-09 17:40:58 -0500

    update

--- Plot1.R
+++ Plot1.R
@@ -5,4 +5,4 @@ data_last<-read.table(data, skip=66637, header=FALSE, nrow=2880, sep=";", na.str
 names(data_last) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 GAP <- data_last[["Global_active_power"]]
 hist(GAP, col="red", main=paste("Global Active Power"), xlab=paste("Global Active Power (kilowatts)"), ylim =c(0,1200))
-# I finally registered my plot image with the appropriate button in Rstudio
+# I finally registered my plot image with the appropriate button in Rstudio and set the image height and width manually with this option
--- Plot2.R
+++ Plot2.R
@@ -5,7 +5,7 @@ data_last<-read.table(data, skip=66637, header=FALSE, nrow=2880, sep=";", na.str
 names(data_last) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 GAP <- data_last[["Global_active_power"]]
 hist(GAP, col="red", main=paste("Global Active Power"), xlab=paste("Global Active Power (kilowatts)"), ylim =c(0,1200))
-# I finally registered my plot image with the appropriate button in Rstudio
+# I finally registered my plot image with the appropriate button in Rstudio and set the image height manually with this option
 
 #code for making plot 2
 #Starting from the code employed for plot 1 - see above for details
@@ -16,6 +16,8 @@ time<-data_last[["Time"]]
 paste_date_time<-paste(datef,time)
 datattime<-as.POSIXct(paste_date_time)
 plot(datattime, GAP, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+# I finally registered my plot image with the appropriate button in Rstudio and set the image height and width manually with this option
+
 
 
 
